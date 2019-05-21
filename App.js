03abd4bb28471f6bb100199d41a552eb5af5fd9e
Akshay Kadam <akshaykadam0904@gmail.com>
2019-05-21 16:48:30 +0530

    Wrap ScrollView

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import {
   PublisherBanner,
 } from 'expo-ads-admob'
 import React, { Component } from 'react'
-import { SafeAreaView } from 'react-native'
+import { SafeAreaView, ScrollView } from 'react-native'
 import { Button, Text } from 'react-native-elements'
 
 const ADUNITID = `ca-app-pub-1425926517331745~6816357585`
@@ -52,31 +52,33 @@ class App extends Component {
   render() {
     const { disableInterstitialBtn, disableRewardedBtn } = this.state
     return (
-      <SafeAreaView style={{ margin: 20 }}>
-        <Text h2>GOOGLE ADMOB DEMO</Text>
-        <Text>
-          Set Ad Unit Id, Interstitial Id & Rewarded Id only on the top level
-          component once.
-        </Text>
-        <Text h4>Banner Ad</Text>
-        <AdMobBanner bannerSize="mediumRectangle" adUnitID={BANNER_ID} />
-        <Text h4>Publisher Banner</Text>
-        <PublisherBanner bannerSize="banner" adUnitID={BANNER_ID} />
-        <Text h4>Interstitial Ad</Text>
-        <Button
-          title="Open"
-          type="outline"
-          disabled={disableInterstitialBtn}
-          onPress={this._openInterstitial}
-        />
-        <Text h4>Rewarded Ad</Text>
-        <Button
-          title="Open"
-          type="outline"
-          disabled={disableRewardedBtn}
-          onPress={this._openRewarded}
-        />
-      </SafeAreaView>
+      <ScrollView>
+        <SafeAreaView style={{ margin: 20 }}>
+          <Text h2>GOOGLE ADMOB DEMO</Text>
+          <Text>
+            Set Ad Unit Id, Interstitial Id & Rewarded Id only on the top level
+            component once.
+          </Text>
+          <Text h4>Banner Ad</Text>
+          <AdMobBanner bannerSize="mediumRectangle" adUnitID={BANNER_ID} />
+          <Text h4>Publisher Banner</Text>
+          <PublisherBanner bannerSize="banner" adUnitID={BANNER_ID} />
+          <Text h4>Interstitial Ad</Text>
+          <Button
+            title="Open"
+            type="outline"
+            disabled={disableInterstitialBtn}
+            onPress={this._openInterstitial}
+          />
+          <Text h4>Rewarded Ad</Text>
+          <Button
+            title="Open"
+            type="outline"
+            disabled={disableRewardedBtn}
+            onPress={this._openRewarded}
+          />
+        </SafeAreaView>
+      </ScrollView>
     )
   }
 }
